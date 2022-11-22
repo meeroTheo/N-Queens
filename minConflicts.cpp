78d02ea0c3400c8b1aa1997726fbb1f77f65ab5e
Frost <frostedwolf789@gmail.com>
2022-11-22 16:38:03 -0500

    fix formatting

--- minConflicts.cpp
+++ minConflicts.cpp
@@ -6,40 +6,41 @@ using std::string;
 
 class minConflicts {
 private:
-    int n;                        // number of queens / board size
-    int *currState = new int[n];  // state of the board
-    int *conflicts = new int[n];  // number of conflicts for each queen
+    int n;                       // number of queens / board size
+    int *currState = new int[n]; // state of the board
+    int *conflicts = new int[n]; // number of conflicts for each queen
 
 public:
     minConflicts(int size, int max_steps) : n(size) {
         bool solved = solve(max_steps);
     }
-    bool solve(int max_steps){
+    bool solve(int max_steps) {
         int colVariable;
-        initialState(); //initial complete assignment for csp
-        for (int i=1;i<max_steps;i++){
+        initialState(); // initial complete assignment for csp
+        for (int i = 1; i < max_steps; i++) {
             setConflicts();
-            if (isSolution()) return true;  //current state is a solution for csp
-            colVariable = colPosition();    //randomly chosen conflicted variable from csp
-            currState[colVariable]=minConflictPos(colVariable); //value (row) which minimizes conflicts
+            if (isSolution())
+                return true;                                      // current state is a solution for csp
+            colVariable = colPosition();                          // randomly chosen conflicted variable from csp
+            currState[colVariable] = minConflictPos(colVariable); // value (row) which minimizes conflicts
         }
         return false;
     }
     void initialState() {
         // modify state
     }
-    int minConflictPos(int colVariable){
-        //row num that has the least number of conflicts
-        //call attacks and put into a list to pick min for rows in range n
-                //like line 11, vconflicts
-        //like line 12 get random pos min(vconflicts)
+    int minConflictPos(int colVariable) {
+        // row num that has the least number of conflicts
+        // call attacks and put into a list to pick min for rows in range n
+        // like line 11, vconflicts
+        // like line 12 get random pos min(vconflicts)
     }
     int colPosition() {
         // random column position given the position has more than 0 conflicts
     }
     void setConflicts() {
         // set of conflicted variables
-        for (int i = 0; i < n; i++) {            // for each variable
+        for (int i = 0; i < n; i++) {                // for each variable
             conflicts[i] = attacks(currState[i], i); // set conflicts based on attacks, state[i] is the row position
         }
     }
@@ -55,35 +56,34 @@ public:
         }
         return attacks;
     }
-    bool isSolution(){
+    bool isSolution() {
         setConflicts();
-        bool solution = true; 
-        for (int i = 0; i < n; i++) {
-            if (conflicts[i] != 0) {
-                solution = false;
-                break;
-            }
-        }
-        return solution;
+        for (int i = 0; i < n; i++)
+            if (conflicts[i] != 0)
+                return false;
+        return true;
     }
-    void printState(int n){
-        //Prints the state in an array form where each index represents board index
-        //Each value represents the row the queen is on
-        for (int i=0; i< n; i++) {
+    void printState(int n) {
+        // Prints the state in an array form where each index represents board index
+        // Each value represents the row the queen is on
+        for (int i = 0; i < n; i++) {
             cout << currState[i] << " ";
         }
         cout << "\n";
     }
 
     void printBoard(int currState) {
-        //Prints the board for visualizing and verifying correctness
-        for (int i = 0; i < (sizeof(n)/ sizeof(int)); i++) {
-            for (int n = 0; n <8; n++) {
-            if (n==i) {
-                cout << "Q" << " "; } 
-            else {
-                cout << "X" << " "; }
+        // Prints the board for visualizing and verifying correctness
+        for (int i = 0; i < (sizeof(n) / sizeof(int)); i++) {
+            for (int n = 0; n < 8; n++) {
+                if (n == i) {
+                    cout << "Q"
+                         << " ";
+                } else {
+                    cout << "X"
+                         << " ";
                 }
+            }
             cout << "\n";
         }
     }
